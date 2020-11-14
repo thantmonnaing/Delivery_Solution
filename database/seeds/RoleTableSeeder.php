2c7cd2bbaaa6fd04cf39c48841aa467899aedfe2
thawzin31 <thawzinwai.ww31@gmail.com>
2020-11-14 15:34:11 +0630

    authentication

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+//use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1=new Role;
+        $role1->name="admin";
+        $role1->save();
+
+        $role2=new Role;
+        $role2->name="deliver";
+        $role2->save();
+
+        $role3=new Role;
+        $role3->name="customer";
+        $role3->save();
+
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'BackendController@home')->name('backend');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
